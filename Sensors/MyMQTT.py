d7b37dc0ddffd5c25813f85794bafc2dbe1347b0
LivioDad <livio.dad@gmail.com>
2025-05-21 00:23:05 +0200

    Docker changes to actual functioning on Rpi5

--- Sensors/MyMQTT.py
+++ Sensors/MyMQTT.py
@@ -1,5 +1,7 @@
 import json
+
 import paho.mqtt.client as PahoMQTT
+
 
 class MyMQTT:
     def __init__(self, clientID, broker, port, notifier):
@@ -10,7 +12,7 @@ class MyMQTT:
         self._topic = ""
         self._isSubscriber = False
         # create an instance of paho.mqtt.client
-        self._paho_mqtt = PahoMQTT.Client(client_id=clientID)
+        self._paho_mqtt = PahoMQTT.Client(client_id=clientID, clean_session=True, userdata=None, protocol=PahoMQTT.MQTTv311, transport="tcp")
         # register the callback
         self._paho_mqtt.on_connect = self.myOnConnect
         self._paho_mqtt.on_message = self.myOnMessageReceived
@@ -23,24 +25,31 @@ class MyMQTT:
         self.notifier.notify(msg.topic, msg.payload)
 
     def myPublish(self, topic, msg):
+        # publish a message with a certain topic
         self._paho_mqtt.publish(topic, json.dumps(msg), 2)
 
     def mySubscribe(self, topic):
-        print ("subscribing to %s" % (topic))
+
+        # subscribe for a topic
         self._paho_mqtt.subscribe(topic, 2)
+        # just to remember that it works also as a subscriber
         self._isSubscriber = True
         self._topic = topic
+        print("subscribed to %s" % (topic))
 
     def start(self):
+        # manage connection to broker
         self._paho_mqtt.connect(self.broker, self.port)
         self._paho_mqtt.loop_start()
 
     def unsubscribe(self):
         if (self._isSubscriber):
+            # remember to unsuscribe if it is working also as subscriber
             self._paho_mqtt.unsubscribe(self._topic)
 
     def stop(self):
         if (self._isSubscriber):
+            # remember to unsuscribe if it is working also as subscriber
             self._paho_mqtt.unsubscribe(self._topic)
 
         self._paho_mqtt.loop_stop()
